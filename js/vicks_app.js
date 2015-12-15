734a721da335042f9ef1b78b6696aa1ba82e1bdc
Vick <eapvic@gmail.com>
2015-12-15 15:15:04 -0800

    landing page with ang routes for registration and login

---
+++ js/vicks_app.js
@@ -0,0 +1,19 @@
+var vicks_app = angular.module('vicks_app', ['ngRoute']);
+
+vicks_app.config(['$routeProvider',
+                 
+    // $routeProvider used to load proper template in index.html
+    function ($routeProvider) {
+        $routeProvider.
+        when('/views/login', {
+            templateUrl: 'views/login.html',
+            
+        }).
+        when('/views/register', {
+            templateUrl: 'views/register.html',
+           
+        }).
+        otherwise({
+            redirectTo: '/views/login'
+        });
+}]);
