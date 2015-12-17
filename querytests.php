d2768ea9d8495d73ec46779daf53c80b2cac2948
[ThinkPad]Ryan Transfiguracion <rtransfig@gmail.com>
2015-12-16 18:22:01 -0800

    Added function didEntryExpire for querytests.php

--- querytests.php
+++ querytests.php
@@ -7,16 +7,43 @@ function doesEntryExist($key, $value)
 	$query_tests =
 	[
 		'email' => "SELECT * FROM users WHERE email='",
-		'display_name' => "SELECT * FROM users WHERE username='"
+		'display_name' => "SELECT * FROM users WHERE username='",
+		'auth_token' => "SELECT * FROM logins WHERE auth_token="
 	];
 
 	if (!isset($query_tests[$key])) return false;
 
 	$query_tests[$key] = $query_tests[$key] . $value . "'";
 
-	if (mysqli_query($conn, $query_tests[$key]) && mysqli_affected_rows($conn) > 0)
+	if (mysqli_query($conn, $query_tests[$key]) && mysqli_num_rows($conn) > 0)
 	{
 		return true;
+	}
+	return false;
+}
+
+function didEntryExpire($key, $value, $duration)
+{
+	require('mysql_connect.php');
+
+	$query_tests =
+	[
+		'auth_token' => "
+			SELECT id, login_timestamp
+			FROM logins WHERE auth_token='"
+	];
+
+	if (!isset($query_tests[$key])) return false;
+
+	$query_tests[$key] = $query_tests[$key] . $value . "'";
+
+	if (mysqli_query($conn, $query_tests[$key]) && mysqli_num_rows($conn) > 0)
+	{
+		while ($row = mysqli_fetch_assoc($result))
+		{
+			if (time() - $row['login_timestamp'] > $duration)
+				return true;
+		}
 	}
 	return false;
 }
