b431626fa4573779340bd5e7dc9c2efa63ba93d4
RhettDelFierro <thinquetank@gmail.com>
2015-12-18 03:09:32 -0800

    delete_blogs query changed

--- delete_blogs.php
+++ delete_blogs.php
@@ -10,29 +10,29 @@ $response = [
     'errors' => []
 ];
 $duration = 600;
+$deleted = 8;
 if (doesEntryExist('auth_token', $auth_token) && !didEntryExpire('auth_token', $auth_token, $duration)) {
 
     foreach ($delete_blog_entries as $key) {
-        $selected_query = "UPDATE blog_infos, blog_texts
-                            JOIN blog_texts ON blog_infos.id = blog_texts.biid
-                            SET blog_infos.time_deleted ='{$time}', blog_infos.status_flags =[value-7]
+        $selected_query = "UPDATE blog_infos
+                            SET time_deleted ='{$time}'
                             WHERE blog_infos.id = '{$key}'";
 
-        mysqli_query($conn,$selected_query);
+        mysqli_query($conn, $selected_query);
 
         if (mysqli_affected_rows($conn) > 0) {
             $response['success'] = true;
-        } else {
-            $response['errors']['delete'] = 'no blog id\'s deleted.';
-        }
+        } else if (!isset($response['errors']['delete'])) {
+                $response['errors']['delete'] = 'some id\'s not deleted.';
+            }
     }
 
     $not_deleted_query = "SELECT id FROM blog_texts
-                          JOIN blog_infos ON blog_infos.id = blog_texts.biid
-                          WHERE blog_infos.time_deleted = ''";
-    $rows = mysqli_query($conn,$not_deleted_query);
+                          JOIN blog_infos ON blog_texts.biid = blog_infos.id
+                          WHERE time_deleted is 'NULL'";
+    $rows = mysqli_query($conn, $not_deleted_query);
 
-    if (mysqli_num_rows($rows) > 0){
+    if (mysqli_num_rows($rows) > 0) {
         $response['success'] = true;
         while ($row = mysqli_fetch_assoc($rows)) {
             array_push($response['data']['id'], $row['id']);
@@ -45,4 +45,5 @@ if (doesEntryExist('auth_token', $auth_token) && !didEntryExpire('auth_token', $
 }
 
 print(json_encode($response));
+
 ?>
